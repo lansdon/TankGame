30f3ed2a8449fa4a4decbf3e28d752a3740d1f2e
Lansdon Page <lansdon.page@email.wsu.edu>
2012-12-14 19:16:51 -0800

    Initial Commit    Project setup - SFML libraries build and linked    Directory structure for local libraries/include/src    Note: Your project will have to link to these local directories

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 TankGame
 ========
+By: Lansdon Page
+Started 12/14/2012
 
 2 Player Tank Game
+
---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
+
+
+
+
+int main() {
+
+
+	std::cout << "Tank Game!\n";
+
+	std::cin.get();
+}
